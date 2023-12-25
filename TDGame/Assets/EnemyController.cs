1289bc25d1b5470d6c5ce2fdd6858f02edb2fbc9
KDKO <kdko.business@gmail.com>
2023-12-25 18:09:06 -0500

    Added comments

--- TDGame/Assets/EnemyController.cs
+++ TDGame/Assets/EnemyController.cs
@@ -13,30 +13,38 @@ public class EnemyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // loop through the children of "WaypointManager" and add them to a list of waypoints
         foreach (Transform waypoint in waypointManager)
         {
             waypoints.Add(waypoint);
         }
+
+        // Set initial position of enemy character to the first waypoiunt
         transform.position = waypoints[0].transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Only move if we have not reached the end of the path
         if (isMoving) {
             Move();
         }
     }
 
     private void Move() {
+        // Make sure we have not reached the last waypoint
         if (waypointIndex < waypoints.Count) {
             if (transform.position == waypoints[waypointIndex].transform.position) {
+                // Check if we are at the waypoint yet; if so, advance the loop to the next item
                 waypointIndex++;
             } else {
+                // Move to the current waypoint in the list
                 transform.position = Vector2.MoveTowards(transform.position, waypoints[waypointIndex].position, Time.deltaTime * moveSpeed);
             }
         }
         else {
+            // Stop moving if we reached the last waypoint
             isMoving = false;
         }
     }
